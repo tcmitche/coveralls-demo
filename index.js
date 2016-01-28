2acfb5234ed048aa4fd1cd62753138fefbb42d1e
Travis Mitchell <contact@traviscmitchell.com>
2016-01-28 10:45:56 -0800

    Important code that must be released now!

--- index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const circle = require('./src/circle.js');
-
-console.log(`The area of a circle of radius 4 is ${circle.area(4)}`);
--- test/circle-spec.js
+++ test/circle-spec.js
@@ -2,15 +2,15 @@
 const assert = require('assert');
 const circle = require('../src/circle.js');
 
-describe('Circle', () => {
-  describe('area', () => {
-    it('should correctly calculate the area of a circle', () => {
+describe('Circle', function() {
+  describe('area', function() {
+    it('should correctly calculate the area of a circle', function() {
         let expected = 50.26548245743669
         assert.equal(circle.area(4), expected)
     });
   });
-  describe('circumference', () => {
-    it('should correctly calculate the circumference of a circle', () => {
+  describe('circumference', function() {
+    it('should correctly calculate the circumference of a circle', function() {
         let expected = 62.83185307179586
         assert.equal(circle.circumference(10), expected)
     })
